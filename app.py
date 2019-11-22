cf1f4260c9de33460896b43751f3cc586485c233
Jordan Stremming <jordan@jordanstremming.com>
2019-11-22 16:12:44 -0600

    better routing test

--- app.py
+++ app.py
@@ -1,35 +1,8 @@
 import os
 import sys
 
-from flask import Flask, render_template, Response, send_from_directory, current_app, Blueprint
+from flask import Flask, render_template, Response, send_from_directory
 from flask_assets import Environment, Bundle
-
-main = Blueprint('main', __name__)
-
-# route to favicon
-@main.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
-
-
-@main.route('/')
-def home():
-    """Page: home of site"""
-    return render_template('index.jinja2')
-
-
-@main.route("/stream/console")
-def console_stream():
-    """Stream: continuous pipeline stream
-    This stream is connected to by client via Javascript to constantly download messages (Responses) from the server.
-    See ./templates/index.jinja2 for Javascript code.
-
-    """
-    def stream():
-        with current_app.app_context:
-            queue = current_app.config['queue']
-            yield queue.get()
-    return Response(stream(), mimetype='text/html')
 
 
 def create_app(queue):
@@ -38,7 +11,6 @@ def create_app(queue):
 
     # create Flask application
     app = Flask(__name__)
-    app.register_blueprint(main)
 
     # render SCSS
     scss = Bundle('site.scss', filters='libsass', output='site.css')
@@ -46,6 +18,29 @@ def create_app(queue):
     assets.url = app.static_url_path
     assets.register('scss_all', scss)
 
-    app.config['queue'] = queue
-    app.app_context().push()
+    # app.config['queue'] = queue
+    # app.app_context().push()
+
+    # route to favicon
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
+
+    @app.route('/')
+    def home():
+        """Page: home of site"""
+        return render_template('index.jinja2')
+
+    @app.route("/stream/console")
+    def console_stream():
+        """Stream: continuous pipeline stream
+        This stream is connected to by client via Javascript to constantly download messages (Responses) from the server.
+        See ./templates/index.jinja2 for Javascript code.
+
+        """
+        def stream():
+            yield queue.get()
+
+        return Response(stream(), mimetype='text/html')
+
     return app
