d1262d6121e3723be725500519c67185233b8f25
Jordan Stremming <jordan@jordanstremming.com>
2019-11-22 15:58:39 -0600

    routing

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import os
 import sys
-from multiprocessing import Queue
 
-from flask import Flask, render_template, Response, send_from_directory, current_app
+from flask import Flask, render_template, Response, send_from_directory, current_app, Blueprint
 from flask_assets import Environment, Bundle
 
+main = Blueprint('main', __name__)
+
 # route to favicon
-@app.route('/favicon.ico')
+@main.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 
-@app.route('/')
+@main.route('/')
 def home():
     """Page: home of site"""
     return render_template('index.jinja2')
 
 
-@app.route("/stream/console")
+@main.route("/stream/console")
 def console_stream():
     """Stream: continuous pipeline stream
     This stream is connected to by client via Javascript to constantly download messages (Responses) from the server.
@@ -37,6 +38,7 @@ def create_app(queue):
 
     # create Flask application
     app = Flask(__name__)
+    app.register_blueprint(main)
 
     # render SCSS
     scss = Bundle('site.scss', filters='libsass', output='site.css')
