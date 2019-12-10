d0bcc8b80124de230c2cba30513a5302ea6300fe
Jordan Stremming <jordan@jordanstremming.com>
2019-12-10 15:43:08 -0600

    Dropping apache2 for waitress

--- app.py
+++ app.py
@@ -54,3 +54,9 @@ def create_app():
         return Response(events(), mimetype='text/plain')
 
     return app
+
+
+if __name__ == '__main__':
+    from waitress import serve
+
+    serve(create_app(), host='0.0.0.0', port='80')
